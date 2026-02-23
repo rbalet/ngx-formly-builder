5830d3301eee6434a17d67037341aa9b5b11bdfb
Raphaël Balet <raphael.balet@outlook.com>
2026-02-23 09:19:12 +0100

    refactor(app): remove unused

--- projects/ngx-formly-builder/src/public-api.ts
+++ projects/ngx-formly-builder/src/public-api.ts
@@ -2,8 +2,8 @@
  * Public API Surface of ngx-formly-builder
  */
 
-export * from './lib/ngx-formly-builder';
-export * from './lib/models/field-group.model';
-export * from './lib/formly-builder.config';
-export { ExportService } from './lib/services/export.service';
 export { EXPORT_SERVICE } from './lib/core/token';
+export { provideFormlyBuilder } from './lib/formly-builder.config';
+export * from './lib/models/field-group.model';
+export { FormlyBuilder } from './lib/ngx-formly-builder';
+export { ExportService } from './lib/services/export.service';
