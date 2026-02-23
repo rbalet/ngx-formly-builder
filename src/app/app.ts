3b036a5aff77b3dc3d862b2cc9a34c923b3041c0
Raphaël Balet <raphael.balet@outlook.com>
2026-02-23 09:23:01 +0100

    docs(app): show custom export service

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,19 @@
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { Component, inject } from '@angular/core';
+import { EXPORT_SERVICE } from 'projects/ngx-formly-builder/src/lib/core/token';
 import { FormlyBuilder } from 'projects/ngx-formly-builder/src/lib/ngx-formly-builder';
 import { FormBuilderService } from 'projects/ngx-formly-builder/src/lib/services/form-builder.service';
+import { CustomExportService } from './custom-export.service';
 
 @Component({
   selector: 'app-root',
   imports: [FormlyBuilder, DragDropModule],
+  providers: [
+    {
+      provide: EXPORT_SERVICE,
+      useClass: CustomExportService,
+    },
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
